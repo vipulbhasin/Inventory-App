001cb872df9690dfa466af4fc9df55a56c37fa7f
Vipul Bhasin <vipul.bhasin@moneymileage.com>
2022-05-10 16:33:02 +0530

    Initial commit

---
+++ src/test/java/com/data/Inventory/App/InventoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.Inventory.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
